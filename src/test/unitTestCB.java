a5bdf430f9340f836d36d01fc01ab25ba9b1aefe
prasadhastantram <prasad.hastantram@gmail.com>
2017-02-06 16:11:15 +0000

    Add files via upload

---
+++ src/test/unitTestCB.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class unitTestCB {
+   @Test
+   public void testAdd() {
+      String str= "Junit is working fine Prasad";
+      assertEquals("Junit is working fine",str);
+   }
+} 
