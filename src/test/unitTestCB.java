26106c1323164b6dc6534375f6ab66ab287792e4
prasadhastantram <prasad.hastantram@gmail.com>
2017-02-07 17:08:16 +0000

    Update unitTestCB.java

--- src/test/unitTestCB.java
+++ src/test/unitTestCB.java
@@ -1,9 +1,15 @@
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
 public class unitTestCB {
-   @Test
-   public void testAdd() {
-      String str= "Junit is working fine Prasad";
-      assertEquals("Junit is working fine",str);
-   }
-} 
+
+	HelloWorld greeter = new HelloWorld();
+
+	@Test
+	public void greeterSaysHello() {
+		assertThat(greeter.sayHello(), containsString("Hello"));
+	}
+
+}
