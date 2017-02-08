016d4c8ea351c198d2c901d41a67f9da93621dcd
prasadhastantram <prasad.hastantram@gmail.com>
2017-02-08 13:30:49 +0000

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public String sayHello() {
-        return "Hello world! WoW!!!";
+        return "Hello world! WoW!!! Hello and Hello";
     }
     public static void main(String[] args) {
         HelloWorld helloWorldObject = new HelloWorld();
